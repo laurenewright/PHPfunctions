747fe31d07297c4f9cd7c2f95261fd0748bab267
lauren wright <wright.lauren20@gmail.com>
2019-04-02 13:40:13 -0400

    added arrayFunctions

---
+++ arrayFunctions.php
@@ -0,0 +1,14 @@
+<?php
+
+$names = array(
+    "Mike" => "frog",
+    "Chris" => "teacher",
+    "Hampton" => "teacher",
+);
+
+foreach(array_keys($names) as $name){
+    echo "Hello, $name!</br>";
+}
+
+
+?>
